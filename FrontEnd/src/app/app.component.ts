56cb6c136e235b5339e56440f97056cf790a5a6d
kamalkantnawal <kamalkantnawal15@gmail.com>
2019-05-20 11:03:25 +0530

    add new line in commopents.ts

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ApiService } from './api.service';
 
+//new line add
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
