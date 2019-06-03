ed6ab7c8009fcab5df3a89fe21f347c05d383d5d
kamalkantnawal <kamalkantnawal15@gmail.com>
2019-06-03 12:43:29 +0530

    #done

--- FrontEnd/src/app/components/navigationbar/inputoutput/inputoutput.component.ts
+++ FrontEnd/src/app/components/navigationbar/inputoutput/inputoutput.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
+import {NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-inputoutput',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputoutputComponent implements OnInit {
 
-  constructor() { }
+  inputoutputdetail: any[] = [];
+  fromdate: string;
+  todate: string;
+  model: NgbDateStruct;
+  flag1 = false;
+  flag2 = false;
+
+  constructor(private api: ApiService, private calendar: NgbCalendar, private parserFormatter: NgbDateParserFormatter) { }
 
   ngOnInit() {
   }
+
+ 
+  getinputoutput = () => {
+     this.api.GetInputOutput(this.fromdate , this.todate).subscribe(
+      data => {
+        this.inputoutputdetail = data;
+        console.table(this.inputoutputdetail);
+      },
+      error => {
+          console.log(error)
+      }     
+     )
+   
+  }
+ 
+  onFromDateSelect($event) {
+ 
+      this.fromdate = this.parserFormatter.format($event);
+      console.log(this.fromdate);
+      this.flag1 = false;
+      
+  }
+ 
+  onToDateSelect($event) {
+      
+     this.todate = this.parserFormatter.format($event);
+     console.log(this.todate);
+     this.flag2 = false;
+     
+  }
+ 
+  showcalender1(){
+ 
+     this.flag1 = true;
+     this.flag2 = false;
+  }
+ 
+  showcalender2(){
+ 
+     this.flag2 = true;
+     this.flag1 = false;
+  }    
+ 
 
 }
