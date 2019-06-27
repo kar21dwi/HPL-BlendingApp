0b79bea45e66434d95125101c8b432df3b7c4d07
kamalkantnawal <kamalkantnawal15@gmail.com>
2019-06-27 20:48:06 +0530

    #css

--- FrontEnd/src/app/components/alltanks/tankno1/tankno1.component.ts
+++ FrontEnd/src/app/components/alltanks/tankno1/tankno1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
 
 @Component({
@@ -15,6 +15,9 @@ export class Tankno1Component implements OnInit {
 	flag = false;
 	rowselector;
 	@Input() simulationflag: boolean;
+	@Input() qualityflag: 0;
+	@Output() message = new EventEmitter();
+
 	qualitylist: any[] = [ 10, 20, 30, 40 ];
 	constructor(private api: ApiService) {
 		this.api.getselectioncount.subscribe((x) => {
